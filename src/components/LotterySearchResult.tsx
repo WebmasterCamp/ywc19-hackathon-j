c8794c723dd49dbad028624a3056efed762debd1
Witnapat <lovelove6985@gmail.com>
2023-07-16 10:39:34 +0700

    fix search bug

--- src/components/LotterySearchResult.tsx
+++ src/components/LotterySearchResult.tsx
@@ -4,13 +4,15 @@ import huays from '@/data/huays';
 
 interface LotterySearchResultsProps {
   numbers: string[];
+  lottoCart: any[];
+  setLottoCart: React.Dispatch<React.SetStateAction<any[]>>;
 }
 
-const LotterySearchResults: FC<LotterySearchResultsProps> = ({ numbers }) => {
-  const [lottoCart, setLottoCart] = useState<any[]>([]);
+const LotterySearchResults: FC<LotterySearchResultsProps> = ({ numbers, lottoCart, setLottoCart }) => {
 
   // TODO: add lotto to cart if not in cart, remove if in cart
   const addLottoToCart = (lotto: any) => {
+    console.log('addLottoToCart')
     if (lottoCart.includes(lotto)) {
       setLottoCart(lottoCart.filter((lottoItem) => lottoItem !== lotto));
     } else {
@@ -22,24 +24,9 @@ const LotterySearchResults: FC<LotterySearchResultsProps> = ({ numbers }) => {
     <>
       <div>
         {numbers.length === 0 ? (
-          <ul className="space-y-2">
-            {huays.map((huay, index) => (
-              <li
-                key={index}
-                onClick={() => addLottoToCart(huay)}
-                className={`transition-all ease-in-out duration-200 w-full rounded px-3 py-4 bg-white border shadow flex gap-0 ${
-                  lottoCart.includes(huay) ? 'bg-green-100' : ''
-                }`}
-              >
-                <div className="w-2/3 space-y-2">
-                  <p>สลากกินแบ่งรัฐบาล</p>
-                  <p className="font-bold text-3xl tracking-[.6em]">{huay}</p>
-                </div>
-                <div className="w-1/3 flex justify-end items-end">
-                  <p className="text-green-600 font-bold text-4xl">80฿</p>
-                </div>
-              </li>
-            ))}
+          <ul className="space-y-2 flex flex-col justify-center items-center">
+            <div>ไม่มีสลากที่คุณต้องการซื้อ</div>
+            <div>มีเลขเด็ดอื่นๆ รอคุณอยู่นะ :) </div>
           </ul>
         ) : (
           <ul className="w-full space-y-2">
@@ -48,7 +35,7 @@ const LotterySearchResults: FC<LotterySearchResultsProps> = ({ numbers }) => {
                 key={index}
                 onClick={() => addLottoToCart(number)}
                 className={`transition-all ease-in-out duration-200 w-full rounded px-3 py-4 bg-white border shadow flex gap-0 ${
-                  lottoCart.includes(number) ? 'bg-green-100' : ''
+                  lottoCart.includes(number) ? 'bg-green-200' : ''
                 }`}
               >
                 <div className="w-2/3 space-y-2">
