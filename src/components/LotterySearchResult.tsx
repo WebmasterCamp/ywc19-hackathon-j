3c2680561c9c33453eb2249ee8a68429ee1d2752
Jeremy <thanawat.j2314@gmail.com>
2023-07-16 09:27:16 +0700

    feat: add huay list

--- src/components/LotterySearchResult.tsx
+++ src/components/LotterySearchResult.tsx
@@ -1,43 +1,74 @@
-import React from 'react';
+import React, { FC, useState } from 'react';
+import huayList from '@/data/huayList';
+import huays from '@/data/huays';
 
 interface LotterySearchResultsProps {
   numbers: string[];
 }
 
-const LotterySearchResults: React.FC<LotterySearchResultsProps> = ({
-  numbers,
-}) => {
+const renderLotto = (lottos: any[]) => {
+  return <></>;
+};
+
+const LotterySearchResults: FC<LotterySearchResultsProps> = ({ numbers }) => {
+  const [lotto, setLotto] = useState<any[]>([]);
+
   return (
-    <div>
-      {numbers.length === 0 ? (
-        <p className="text-center p-4">No results found.</p>
-      ) : (
-        <ul className="w-full">
-          {numbers.map((number, index) => (
-            <li
-              key={index}
-              className="hover:scale-105 transition-all ease-in-out duration-200 w-full rounded px-3 py-4 bg-white border shadow flex gap-0"
-            >
-              <div className="w-1/2">
-                <p>สลากกินแบ่งรัฐบาล</p>
-                <p className="font-bold text-3xl tracking-[.6em]">{number}</p>
-              </div>
-              <div className="w-1/6 flex flex-col items-center justify-end text-sm">
-                <p>งวดวันที่</p>
-                <p className="">16</p>
-              </div>
-              <div className="w-1/6 flex flex-col items-center justify-end text-sm">
-                <p>ชุดที่</p>
-                <p className="">4</p>
-              </div>
-              <div className="w-1/6 flex justify-end items-end">
-                <p className="text-green-600 font-bold text-4xl">80฿</p>
-              </div>
-            </li>
-          ))}
-        </ul>
-      )}
-    </div>
+    <>
+      <div>
+        {numbers.length === 0 ? (
+          <ul className="space-y-2">
+            {huays.map((huay, index) => (
+              <li
+                key={index}
+                className="transition-all ease-in-out duration-200 w-full rounded px-3 py-4 bg-white border shadow flex gap-0"
+              >
+                <div className="w-1/2">
+                  <p>สลากกินแบ่งรัฐบาล</p>
+                  <p className="font-bold text-3xl tracking-[.6em]">{huay}</p>
+                </div>
+                <div className="w-1/6 flex flex-col items-center justify-end text-sm">
+                  <p>งวดวันที่</p>
+                  <p className="">16</p>
+                </div>
+                <div className="w-1/6 flex flex-col items-center justify-end text-sm">
+                  <p>ชุดที่</p>
+                  <p className="">4</p>
+                </div>
+                <div className="w-1/6 flex justify-end items-end">
+                  <p className="text-green-600 font-bold text-4xl">80฿</p>
+                </div>
+              </li>
+            ))}
+          </ul>
+        ) : (
+          <ul className="w-full">
+            {numbers.map((number, index) => (
+              <li
+                key={index}
+                className="transition-all ease-in-out duration-200 w-full rounded px-3 py-4 bg-white border shadow flex gap-0"
+              >
+                <div className="w-1/2">
+                  <p>สลากกินแบ่งรัฐบาล</p>
+                  <p className="font-bold text-3xl tracking-[.6em]">{number}</p>
+                </div>
+                <div className="w-1/6 flex flex-col items-center justify-end text-sm">
+                  <p>งวดวันที่</p>
+                  <p className="">16</p>
+                </div>
+                <div className="w-1/6 flex flex-col items-center justify-end text-sm">
+                  <p>ชุดที่</p>
+                  <p className="">4</p>
+                </div>
+                <div className="w-1/6 flex justify-end items-end">
+                  <p className="text-green-600 font-bold text-4xl">80฿</p>
+                </div>
+              </li>
+            ))}
+          </ul>
+        )}
+      </div>
+    </>
   );
 };
 
--- src/pages/huay.tsx
+++ src/pages/huay.tsx
@@ -1,18 +1,26 @@
 import LotteryInput from '@/components/LotteryInput';
 import LotterySearchResults from '@/components/LotterySearchResult';
 import { NextPage } from 'next';
-import React, { useEffect, useState } from 'react';
-import { Noto_Sans_Thai_Looped } from 'next/font/google';
+import { useState } from 'react';
+import { Noto_Sans_Thai_Looped, Bai_Jamjuree } from 'next/font/google';
+import huays from '@/data/huays';
 
 const nonoSansThaiLooped = Noto_Sans_Thai_Looped({
   weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
   subsets: ['latin'],
 });
 
+const baiJamjuree = Bai_Jamjuree({
+  weight: ['400', '700'],
+  style: ['normal', 'italic'],
+  subsets: ['latin'],
+  display: 'swap',
+});
+
 const HomePage: NextPage = () => {
   const [searchResults, setSearchResults] = useState<string[]>([]);
 
-  const numberArray = ['123456', '234567', '345678'];
+  const numberArray = huays;
 
   const handleSearch = (array: string[]): null | void => {
     if (array.every((digit) => digit === '')) {
@@ -33,20 +41,14 @@ const HomePage: NextPage = () => {
         setSearchResults([number]);
       }
     });
-
-    // console.log('no match');
     return null;
   };
 
-  // useEffect(() => {
-  //   console.log('result', searchResults);
-  // }, [searchResults]);
-
   return (
     <div
-      className={`container mx-auto p-10 flex flex-col h-screen items-center justify-start max-w-lg bg-gray-50/10 ${nonoSansThaiLooped.className}`}
+      className={`container mx-auto p-10 flex flex-col h-screen items-center justify-start max-w-lg bg-gray-50/10 ${baiJamjuree.className}`}
     >
-      <div className="flex flex-col gap-2 w-full">
+      <div className="flex flex-col gap-2 w-full h-full ">
         <h1 className="text-xl font-semibold">ค้นหาเลขเด็ด</h1>
         <p>งวดวันที่ 1 กรกฎาคม 2566</p>
         <LotteryInput onSubmit={handleSearch} />
