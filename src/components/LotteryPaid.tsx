885bad59221a307e9e43e0f2106419d407a825c5
Jeremy <thanawat.j2314@gmail.com>
2023-07-16 11:59:54 +0700

    test

--- src/components/LotteryPaid.tsx
+++ src/components/LotteryPaid.tsx
@@ -2,6 +2,7 @@ import React, { FC, useState } from 'react';
 import huayList from '@/data/huayList';
 import huays from '@/data/huays';
 import Image from 'next/image';
+import Link from 'next/link';
 
 interface LotterPaidProps {
   lottoCart: any[];
@@ -9,13 +10,16 @@ interface LotterPaidProps {
   setHuayState: React.Dispatch<React.SetStateAction<number>>;
 }
 
-const LotteryPaid: FC<LotterPaidProps> = ({ lottoCart, setLottoCart, setHuayState }) => {
-
-  const [done,setDone] = useState(false)
+const LotteryPaid: FC<LotterPaidProps> = ({
+  lottoCart,
+  setLottoCart,
+  setHuayState,
+}) => {
+  const [done, setDone] = useState(false);
 
   // TODO: add lotto to cart if not in cart, remove if in cart
   const addLottoToCart = (lotto: any) => {
-    console.log('addLottoToCart')
+    console.log('addLottoToCart');
     if (lottoCart.includes(lotto)) {
       setLottoCart(lottoCart.filter((lottoItem) => lottoItem !== lotto));
     } else {
@@ -23,29 +27,23 @@ const LotteryPaid: FC<LotterPaidProps> = ({ lottoCart, setLottoCart, setHuayStat
     }
   };
 
-  if (done == false){
+  if (done == false) {
     return (
       <>
         <div>
-          <div className='flex justify-center items-center '>
-            <div className=' text-xl font-bold'>
-              ยืนยันการชำระเงิน
+          <div className="flex justify-center items-center ">
+            <div className=" text-xl font-bold">ยืนยันการชำระเงิน</div>
+          </div>
+          <div className="flex justify-between items-center my-4">
+            <div className=" text-xl ">จำนวนฉลาก</div>
+            <div className=" text-xl flex flex-row gap-2">
+              <div className=" font-bold">{lottoCart.length}</div> ใบ
             </div>
           </div>
-          <div className='flex justify-between items-center my-4'>
-            <div className=' text-xl '>
-              จำนวนฉลาก
-            </div>
-            <div className=' text-xl flex flex-row gap-2'>
-              <div className=' font-bold'>{lottoCart.length}</div> ใบ
-            </div>
-          </div>
-          <div className='flex justify-between items-center my-4'>
-            <div className=' text-xl '>
-              รวมทั้งหมด
-            </div>
-            <div className=' text-xl flex flex-row gap-2'>
-              <div className=' font-bold'>{95 * lottoCart.length}</div> บาท
+          <div className="flex justify-between items-center my-4">
+            <div className=" text-xl ">รวมทั้งหมด</div>
+            <div className=" text-xl flex flex-row gap-2">
+              <div className=" font-bold">{95 * lottoCart.length}</div> บาท
             </div>
           </div>
           <Image
@@ -55,42 +53,37 @@ const LotteryPaid: FC<LotterPaidProps> = ({ lottoCart, setLottoCart, setHuayStat
             width={500}
             height={500}
             priority
-            onClick={(e) => { setDone(true) }}
+            onClick={(e) => {
+              setDone(true);
+            }}
           />
         </div>
       </>
     );
-  }
-  else {
+  } else {
     return (
-    <>
-      <div>
-        <div className='flex justify-center items-center'>
-          <div className=' text-2xl font-bold'>
-            ชำระเงินเสร็จสิ้น
+      <>
+        <div>
+          <div className="flex justify-center items-center">
+            <div className=" text-2xl font-bold">ชำระเงินเสร็จสิ้น</div>
+          </div>
+          <div className="flex justify-between items-center my-4">
+            <div className=" text-xl ">จำนวนฉลาก</div>
+            <div className=" text-xl flex flex-row gap-2">
+              <div className=" font-bold">{lottoCart.length}</div> ใบ
+            </div>
+          </div>
+          <div className="flex justify-between items-center my-4">
+            <div className=" text-xl ">รวมทั้งหมด</div>
+            <div className=" text-xl flex flex-row gap-2">
+              <div className=" font-bold">{95 * lottoCart.length}</div> บาท
+            </div>
+            <Link href="/">ออก</Link>
           </div>
         </div>
-        <div className='flex justify-between items-center my-4'>
-          <div className=' text-xl '>
-            จำนวนฉลาก
-          </div>
-          <div className=' text-xl flex flex-row gap-2'>
-            <div className=' font-bold'>{lottoCart.length}</div> ใบ
-          </div>
-        </div>
-        <div className='flex justify-between items-center my-4'>
-          <div className=' text-xl '>
-            รวมทั้งหมด
-          </div>
-          <div className=' text-xl flex flex-row gap-2'>
-            <div className=' font-bold'>{95 * lottoCart.length}</div> บาท
-          </div>
-        </div>
-      </div>
-    </>
-  );
+      </>
+    );
   }
-  
 };
 
 export default LotteryPaid;
